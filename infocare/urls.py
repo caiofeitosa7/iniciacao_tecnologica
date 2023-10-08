849c02f650c88e9290078e46b3b79cf6d83fa4f4
Caio Feitosa <franciscocaiof@gmail.com>
2023-10-08 11:55:13 -0300

    Criada a página de login

--- infocare/urls.py
+++ infocare/urls.py
@@ -4,7 +4,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.login, name='login'),
     path('home/', views.home, name='home'),
     path('home/pagina_inicial/', views.pagina_inicial),
-    path('home/usuarios/', include('usuario.urls'))
+    path('home/usuarios/', include('usuario.urls')),
+    path('imagem/<int:cod_img>', views.imagem_local, name='imagem'),
 ]
--- infocare/views.py
+++ infocare/views.py
@@ -1,6 +1,16 @@
+import os
+
+from django.http import FileResponse
+from django.templatetags.static import static
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.template.loader import render_to_string
+from . import settings
+
+
+def login(request):
+    imagem_url = imagem_local(request, 0)
+    return render(request, 'login.html', {'img_url': imagem_url})
 
 
 def home(request):
@@ -9,6 +19,18 @@ def home(request):
 
 def pagina_inicial(request):
     return JsonResponse({'html': [render_to_string('pagina_inicial.html')]})
+
+
+def imagem_local(request, cod_img):
+    if not cod_img:
+        imagem_url = os.path.join('templates', 'images', 'bg-login.png')
+
+    return FileResponse(open(imagem_url, 'rb'))
+
+
+
+
+
 
 
 
