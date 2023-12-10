7f9b5e49d71f509e2cf17e51dc79259dcb17b4cc
Caio Feitosa <franciscocaiof@gmail.com>
2023-12-10 15:53:26 -0300

    Criacao do banco de dados

--- infocare/settings.py
+++ infocare/settings.py
@@ -108,11 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'pt-BR'
-
 TIME_ZONE = 'America/Fortaleza'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
